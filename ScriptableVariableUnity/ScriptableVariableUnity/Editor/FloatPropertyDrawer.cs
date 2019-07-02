73256c43068cad0a6b2150b9921da7b9ce3aafe8
Sean Keiffer Lee <seankeiffer@gmail.com>
2019-07-02 14:43:30 +0800

    [+] Removed debug.log from property drawers

--- ScriptableVariableUnity/ScriptableVariableUnity/Editor/FloatPropertyDrawer.cs
+++ ScriptableVariableUnity/ScriptableVariableUnity/Editor/FloatPropertyDrawer.cs
@@ -12,9 +12,6 @@ public class FloatPropertyDrawer : PropertyDrawer
     {
         EditorGUI.BeginProperty(position, label, property);
 
-        Debug.Log(position.position);
-        Debug.Log(position.size);
-
         position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
         boolVal = EditorGUI.Popup(new Rect(position.position - new Vector2(20, 0), new Vector2(17.5f, position.size.y)), boolVal, options);
         bool useConstant = property.FindPropertyRelative("useConstant").boolValue = (boolVal == 0);
